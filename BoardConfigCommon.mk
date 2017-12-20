110674d37bac9cfd3eb4c1167724bced0c652464
PsyMan47 <xhonirexhepi@gmail.com>
2017-12-20 22:09:05 +0000

    Bring back TARGET_RIL_VARIANT := caf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,6 +173,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 # RIL
 BOARD_RIL_CLASS := ../../../$(VENDOR_PATH)/ril
 PROTOBUF_SUPPORTED := true
+TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
