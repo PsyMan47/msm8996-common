6ac70566f24757d409d5e9e06e89659ed775ca13
PsyMan47 <xhonirexhepi@gmail.com>
2018-04-28 21:06:33 +0000

    msm8996-common: Move ConfigPanel and PocketMode to /vendor

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_REQUIRED_MODULES := XiaomiPocketMode
 
@@ -23,7 +23,6 @@ LOCAL_RESOURCE_DIR := \
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_PACKAGE)
